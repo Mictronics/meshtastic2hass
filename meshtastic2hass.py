b339111a895a5ad4a514a488af802c84d3ae822b
Mictronics <github@mictronics.de>
2024-01-13 21:23:38 +0100

    Added copyright.

--- meshtastic2hass.py
+++ meshtastic2hass.py
@@ -2,6 +2,8 @@
 
 # This file is part of Meshtastic to Home Assistant (Hass)
 #
+# Copyright (c) 2024 Michael Wolf <michael@mictronics.de>
+#
 # meshtastic2hass is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
